7d1d4680404f0193ae7e86db6cc8733b372446a5
Matěj Hýl <mates.hyl@seznam.cz>
2020-11-29 14:54:12 +0100

    Pridani stranek a navigace

--- SupaQRreader/SupaQRreader/App.xaml.cs
+++ SupaQRreader/SupaQRreader/App.xaml.cs
@@ -10,7 +10,7 @@ namespace SupaQRreader
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new TabbedPage1();
         }
 
         protected override void OnStart()
