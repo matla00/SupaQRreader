ef97aa4dd02f94d40dbd9597e61300aaa42573b3
matla00 <mates.hyl@seznam.cz>
2020-11-30 14:55:50 +0100

    Pridani skenovaci stranky a povoleni kamery

--- SupaQRreader/SupaQRreader/QRreader.xaml.cs
+++ SupaQRreader/SupaQRreader/QRreader.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ZXing;
 
 namespace SupaQRreader
 {
@@ -15,6 +16,13 @@ namespace SupaQRreader
         public QRreader()
         {
             InitializeComponent();
+        }
+        public void scanView_OnScanResult(Result result)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await DisplayAlert("Scanned result", "The barcode's text is " + result.Text + ". The barcode's format is " + result.BarcodeFormat, "OK");
+            });
         }
     }
 }
